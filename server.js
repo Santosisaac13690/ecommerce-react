cdb4db6a645657b0c28aa5a6388797f639bba1d3
Isaac Santos <Isaac@R2Hs-MBP.attlocal.net>
2022-03-31 09:12:18 -0400

    form db edits

--- server.js
+++ server.js
@@ -41,6 +41,20 @@ app.post("/api/insert", (req, res) => {
     });
 });
 
+// ========= FORM DB ========= //
+
+app.post("/api/insert", (req, res) => {
+    const userName = req.body.user_name;
+    const userEmail = req.body.user_email;
+    const userPhoneNumber = req.body.user_phone_number;
+    const userMessage = req.body.user_message;
+
+    const sqlInsert = "INSERT INTO user_contact_form (user_name, user_email, user_phone_number, user_message) VALUES (?, ?, ?, ?);";
+    db.query(sqlInsert, [userName, userEmail, userPhoneNumber, userMessage], (err, result) => {
+        console.log(err)
+    });
+});
+
 app.use(express.static(path.join(__dirname, './build')));
 
 app.listen(process.env.PORT || 3001, () => {
--- src/components/MainPages/Contact.jsx
+++ src/components/MainPages/Contact.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import Nav from '../Reusable/Nav'
 import Footer from '../Reusable/Footer';
+import axios from 'axios';
 
 function Contact() {
+
+  const [userName, setName] = useState("");
+  const [userEmail, setEmail] = useState("");
+  const [userPhoneNumber, setPhoneNumber] = useState("");
+  const [userMessage, setMessage] = useState("");
+
+  const submitEmail = () => {
+    axios.post("https://isaac-stayquiet.herokuapp.com/api/insert", {
+        user_name: userName, 
+        user_email: userEmail,
+        user_phone_number: userPhoneNumber,
+        user_message: userMessage,
+      }).then(() => {
+      alert('succesful insert');
+    });
+  };
+
   return (
     <div>
       <Nav />
@@ -39,28 +57,36 @@ function Contact() {
               <div className="inputBox">
                 <label>
                 Name &#42;
-                <input type="text" name="contact" required="required" />
+                <input type="text" name="contact" required="required" onChange={(e) => {
+                    setName(e.target.value);
+                  }}/>
                 </label>
               </div>
     
               <div className="inputBox">
                 <label>
                 Email &#42;
-                <input type="email" name="contact" required="@" />
+                <input type="email" name="contact" required="@" onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}/>
                 </label>
               </div>
     
               <div className="inputBox">
                 <label>
                 Number
-                <input type="text" name="contact" />
+                <input type="text" name="contact" onChange={(e) => {
+                    setPhoneNumber(e.target.value);
+                  }}/>
                 </label>
               </div>
     
               <div className="inputBox">
                 <label>
                 Message &#42;
-                <textarea name="contact" required="required"></textarea>
+                <textarea name="contact" required="required" onChange={(e) => {
+                    setMessage(e.target.value);
+                  }}></textarea>
                 </label>
               </div>
     
